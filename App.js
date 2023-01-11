1d7ef9d97ec2ac8e9710891a65f68926126d9197
fxddev <79515689+fxddev@users.noreply.github.com>
2023-01-11 04:25:45 +0000

    up

--- App.js
+++ App.js
@@ -1,11 +1,26 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import PocketBase from 'pocketbase';
+
+const pb = new PocketBase('https://ecommerce.choniki.tk');
+
 
 export default function App() {
+
+  getProducts()
+
+  async function getProducts() {
+    const records = await pb.collection('products').getFullList(200 /* batch size */, {
+      sort: '-created',
+    });
+
+    alert(JSON.stringify(records))
+  }
+
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
-      <Text>Hello tecno!</Text>
+      <Text>Hello tecno bro!</Text>
       <StatusBar style="auto" />
     </View>
   );
